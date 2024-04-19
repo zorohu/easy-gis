82b84c3ad9c16d683d0e748784307d4b7bb07231
author.zero <zorokakao@gmail.com>
2024-04-19 15:15:01 +0800

    feat: 增加矢量文件坐标转换

--- shp.py
+++ shp.py
@@ -1,6 +1,10 @@
+import fiona
 import geopandas as gpd
 from pyproj import Transformer
 from shapely import Point
+from shapely.ops import transform
+
+from transform import Transform
 
 
 def geojson_to_shp(geojson_file, shp_file):
@@ -103,10 +107,62 @@ def project_coordinates(input_file, output_file, input_crs, output_crs):
     transformer = Transformer.from_crs(input_crs, output_crs, always_xy=True)
 
     # 进行坐标转换
-    gdf['geometry'] = gdf['geometry'].to_crs(output_crs)
+    gdf['geometry'] = gdf['geometry'].apply(lambda geom: transform(transformer.transform, geom))
 
     # 将转换后的结果保存为新文件
     gdf.to_file(output_file, driver='ESRI Shapefile')
+
+
+def recur_map(f, data):
+    """递归处理所有坐标
+
+    Arguments:
+        f {function} -- [apply function]
+        data {collection} -- [fiona collection]
+    """
+
+    return [not type(x) is list and f(x) or recur_map(f, x) for x in data]
+
+
+def project_convertor(src_path, dst_path, convert_type):
+    """convert input china coordinate to another.
+
+\b
+    Arguments:
+        convert_type {string} -- [coordinate convert type, e.g. wgs2bd]
+\b
+            wgs2gcj : convert WGS-84 to GCJ-02
+            wgs2bd  : convert WGS-84 to DB-09
+            gcj2wgs : convert GCJ-02 to WGS-84
+            gcj2bd  : convert GCJ-02 to BD-09
+            bd2wgs  : convert BD-09 to WGS-84
+            bd2gcj  : convert BD-09 to GCJ-02
+
+        src_path {string} -- [source file path]
+        dst_path {string} -- [destination file path]
+
+
+    Example:
+
+\b
+        coord_covert wgs2gcj ./test/data/line/multi-polygon.shp ~/temp/qqqq.shp
+    """
+
+    with fiona.open(src_path, 'r', encoding='utf-8') as source:
+        source_schema = source.schema.copy()
+        with fiona.open(dst_path, 'w', encoding='utf-8', **source.meta) as out:
+            transform = Transform()
+            f = lambda x: getattr(transform, convert_type)(x[0], x[1])  #dynamic call convert func
+
+            for fea in source:
+                collections = fea['geometry']['coordinates']
+                if type(collections) is tuple:
+                    fea['geometry']['coordinates'] = f(collections)
+                elif type(collections) is list:
+                    fea['geometry']['coordinates'] = recur_map(f, collections)
+                else:
+                    raise TypeError("collection must be list or tuple")
+                out.write(fea)
 
 
 if __name__ == '__main__':
@@ -136,7 +192,7 @@ if __name__ == '__main__':
     # 指定输入文件、输出文件、输入坐标系和输出坐标系
     input_file = "data/test.shp"
     output_file = "output.shp"
-    input_crs = "EPSG:4490"  # WGS84 经纬度坐标系
+    # input_crs = "EPSG:4490"  # WGS84 经纬度坐标系
     output_crs = "EPSG:3857"  # Web Mercator 投影坐标系
     # 执行投影转换
-    project_coordinates(input_file, output_file, input_crs, output_crs)
+    project_convertor(input_file, output_file, "wgs2gcj")
